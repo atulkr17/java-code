65b86aae20008a63dabef7155d136d42d8c7bf56
atulkr17 <144064361+atulkr17@users.noreply.github.com>
2023-09-05 02:56:52 +0530

    Add files via upload

---
+++ Even_or_odd_number.java
@@ -0,0 +1,17 @@
+public class Even_or_odd_number
+{
+   public static void main(String[] args)
+    {
+        int x=56;
+        int y=9;
+        if(x%2==0)
+        {
+            System.out.println("even number");
+        }
+        else
+        {
+            System.out.println("odd number");
+        }
+    }
+    
+}
